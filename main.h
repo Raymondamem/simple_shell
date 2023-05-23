8ae3194a6ed22e36ca5cb5158984a2de7d4a927b
Raymondamem <raymondamem525@gmail.com>
2023-05-23 23:56:21 +0100

    added func to free memory to main.h

--- main.h
+++ main.h
@@ -14,6 +14,6 @@ int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 pid_t _fork(void);
-
+int free_multiple(int count, ...);
 
 #endif /* __MAIN_H__ */
