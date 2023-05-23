0e75d557d5ae6c3509e0be6cd69fad0dc4c93fd7
Raymondamem <raymondamem525@gmail.com>
2023-05-23 20:26:05 +0100

    added _strcpy

---
+++ _strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+char *_strcpy(char *dest, const char *src)
+{
+    char *temp = dest;
+
+    while (*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+
+    return temp;
+}
--- main.h
+++ main.h
@@ -12,6 +12,7 @@ char *_getenv(char *variable);
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 pid_t _fork(void);
 
 
